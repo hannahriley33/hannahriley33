7dc35f66ded38b27ff7862aa5a4ad2439461169d
Hannah Riley <riley.hannahm@gmail.com>
2020-03-31 16:32:58 -0700

    2 post tests passing

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -5,50 +5,36 @@ const app = require('../lib/app');
 
 describe('post routes', () => {
   it('creates a new post', async() => {
-  const user = await getUser()
+    const user = await getUser({ username: 'pajamas' });
     return getAgent()
-      .post('/api/v1/auth/signup')
+      .post('/api/v1/posts')
       .send({
-        caption: 'TEST caption',
-        passwordHash: 'hannahsPassword',
-        tags: ['tag'],
-        user: user._id'
+        caption: 'this is a caption',
+        photoUrl: 'myPhoto.url',
+        tags: ['selfie', 'angles', 'wfh'],
+        user: user._id
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
-          username: expect.any(String),
+          caption: expect.any(String),
+          photoUrl: expect.any(String),
+          tags: [expect.any(String), expect.any(String), expect.any(String)],
+          user: expect.any(String),
           __v: 0
         });
       });
   });
-
-//   it('logs in a user', async() => {
-//     const user = await getUser();
-//     return request(app)
-//       .post('/api/v1/auth/login')
-//       .send({
-//         username: 'pajamas',
-//         password: 'pajamasallday'
-//       })
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           _id: expect.any(String),
-//           username: expect.any(String),
-//           __v: 0
-//         });
-//       });
-//   });
-
-//   it('verifies a logged in user', () => {
-//     return getAgent()
-//       .get('/api/v1/auth/verify')
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           _id: expect.any(String),
-//           username: expect.any(String),
-//           __v: 0
-//         });
-//       });
-//   });
+  it('gets a post by id', async() => {
+    const user = await getUser({ username: 'pajamas' });
+    const post = await getPost({ user: user._id });
+    return getAgent()
+      .get(`/api/v1/posts/${post._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          ...post,
+          user: user
+        });
+      });
+  });
 });
--- db/seed.js
+++ db/seed.js
@@ -4,7 +4,7 @@ const User = require('../lib/models/User');
 const Post = require('../lib/models/Post');
 // const Note = require('../lib/models/Note');
 
-module.exports = async({ usersToCreate = 15, notesToCreate = 50 } = {}) => {
+module.exports = async({ usersToCreate = 15, notesToCreate = 50, postsToCreate = 25 } = {}) => {
   const loggedInUser = await User.create({
     username: 'pajamas',
     password: 'pajamasallday'
@@ -15,17 +15,16 @@ module.exports = async({ usersToCreate = 15, notesToCreate = 50 } = {}) => {
     password: chance.name()
   })));
 
-  // await Post.create([...Array(postsToCreate)].map(() => ({
-  //   photoUrl: chance.profession(),
-  //   tags: [chance.name(), chance.name(), chance.animal()],
-  //   caption: chance.sentence(),
-  //   user: chance.weighted([loggedInUser, ...users], [2, ...users.map(() => 1)])._id
-  // })));
-};
+  await Post.create([...Array(postsToCreate)].map(() => ({
+    caption: chance.sentence(),
+    photoUrl: chance.url(),
+    tags: [chance.animal(), chance.animal(), chance.word()],
+    user: chance.weighted([loggedInUser, ...users], [2, ...users.map(() => 1)])._id
+  })));
 
 // await Note.create([...Array(notesToCreate)].map(() => ({
 //   title: chance.profession(),
 //   body: chance.sentence(),
 //   author: chance.weighted([loggedInUser, ...users], [2, ...users.map(() => 1)])._id
 // })));
-
+};
