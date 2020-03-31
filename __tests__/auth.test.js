6373f0a3a2adcd3a11f9be0b690e1114ed230cc7
Hannah Riley <riley.hannahm@gmail.com>
2020-03-31 14:17:32 -0700

    first auth test passing

---
+++ __tests__/auth.test.js
@@ -0,0 +1,50 @@
+const { getUser, getAgent } = require('../db/data-helpers');
+const User = require('../lib/models/User');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('auth routes', () => {
+  it('signs up a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        username: 'hannah',
+        passwordHash: 'hannahsPassword'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          username: expect.any(String),
+          __v: 0
+        });
+      });
+  });
+
+  //   it('logs in a user', async() => {
+  //     return request(app)
+  //       .post('/api/v1/auth/login')
+  //       .send({
+  //         username: 'pajamas',
+  //         password: 'pajamasallday'
+  //       })
+  //       .then(res => {
+  //         expect(res.body).toEqual({
+  //           _id: expect.any(String),
+  //           email: 'pajamas',
+  //           __v: 0
+  //         });
+  //       });
+  //   });
+
+//   it('verifies a logged in user', () => {
+//     return getAgent()
+//       .get('/api/v1/auth/verify')
+//       .then(res => {
+//         expect(res.body).toEqual({
+//           _id: expect.any(String),
+//           email: 'test@test.com',
+//           __v: 0
+//         });
+//       });
+//   });
+});
---
+++ db/seed.js
@@ -0,0 +1,22 @@
+  
+const chance = require('chance').Chance();
+const User = require('../lib/models/User');
+// const Note = require('../lib/models/Note');
+
+module.exports = async({ usersToCreate = 15, notesToCreate = 50 } = {}) => {
+  const loggedInUser = await User.create({
+    username: 'test@test.com',
+    password: 'password'
+  });
+
+  const users = await User.create([...Array(usersToCreate)].slice(1).map(() => ({
+    username: chance.email(),
+    password: chance.name()
+  })));
+
+  // await Note.create([...Array(notesToCreate)].map(() => ({
+  //   title: chance.profession(),
+  //   body: chance.sentence(),
+  //   author: chance.weighted([loggedInUser, ...users], [2, ...users.map(() => 1)])._id
+  // })));
+};
